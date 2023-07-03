cf0a9f98fbf638f214a8ed87e4b815424cb72176
esciafardini <edward.ciafardini@aclaimant.com>
2023-07-03 10:06:47 -0400

    Complete exercises 3.19, 3.20, preparing to deploy

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
+const Person = require('./models/person')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -15,7 +18,7 @@ const requestLogger = (request, response, next) => {
 }
 
 //custom morgan logging token
-morgan.token('body', function getId (req) {
+morgan.token('body', function getId(req) {
   return JSON.stringify(req.body)
 })
 
@@ -26,117 +29,71 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(requestLogger)
 
-
-const port = 3999
-
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
-app.get('/', (req, res) => {
-  res.send(`Yea, that's the biznuss`)
+app.get('/info', (req, res, next) => {
+  res.send(`<h1>Informative Daniels</h1>`)
 })
 
-app.get('/info', (req, res) => {
-
-  let date = new Date()
-  let count = persons.length
-
-  res.send(`<p>phonebook has info for ${count} ${count === 1 ? 'person' : 'people'}</p><p>${date}</p>`)
+app.get('/api/persons', (_req, res, next) => {
+  Person.find({})
+    .then(people => { res.json(people) })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons', (req, res) => {
-  res.send(persons)
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => { res.json(person) })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  let person = persons.find(p => p.id === parseInt(req.params.id))
-
-  if (person) {
-    res.send(person)
-  } else {
-    res.sendStatus(404).send(`No person found with id ${req.params.id}`);
-  }
+app.put('/api/persons/:id', (req, res, next) => {
+  //at this time, we only allow users to update phone numbers
+  const personObjectId = new mongoose.Types.ObjectId(req.params.id)
+  Person.findOneAndUpdate(
+    { "_id": personObjectId }, //find by id
+    { "number": req.body.number },
+    { runValidators: true }
+  ) //update the phone number
+    .then(person => { res.json(person) })
+    .catch(error => next(error))
 })
 
-
-app.post('/api/persons', (req, res) => {
-  let id = persons.map(p => p.id).reduce((x, y) => Math.max(x, y), 1) + 1
-  let newPerson = { ...req.body, "id": id }
-
-
-
-  let errors = {
-    "missing number": !newPerson.number,
-    "missing name": !newPerson.name,
-    "name already exists": persons.find(p => p.name === newPerson.name) ? true : false
-  }
-
-  let getErrors = (errs) => {
-    const errorsArray = [];
-
-    for (const key in errs) {
-      if (errs.hasOwnProperty(key) && errs[key] === true) {
-        errorsArray.push(key);
-      }
-    }
-
-    return errorsArray;
-  }
-
-  let errorArray = getErrors(errors)
-
-  if (newPerson && errorArray.length === 0) {
-    persons = persons.concat(newPerson)
-    //
-    //automatically sets the appropriate headers in the response to indicate that the data being sent is in JSON format.
-    res.json(newPerson)
-    //automatically sets the appropriate headers in the response to indicate that the data being sent is in JSON format.
-    //
-  } else {
-    return res.status(404).json({
-      errors: errorArray
-    })
-  }
+app.post('/api/persons', (req, res, next) => {
+  Person.create(req.body)
+    .then(person => { res.json(person) })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-
-  let id = parseInt(req.params.id)
-  let person = persons.find(p => p.id === id)
-
-  if (person) {
-    persons = persons.filter(p => p.id !== id)
-    res.sendStatus(200).send('Deleted a motherfucker')
-  } else {
-    res.sendStatus(404).send(`No person found with id ${id}`)
-  }
+app.delete('/api/persons/:id', (req, res, next) => {
+  const personObjectId = new mongoose.Types.ObjectId(req.params.id)
+  Person.deleteOne({ "_id": personObjectId })
+    .then(person => { res.json(person) })
+    .catch(error => next(error))
 })
 
+// error and exception middlewares
 app.use((req, res, next) => {
   res.status(404).send("This route doesn't exist")
 })
 
-app.listen(port, () => {
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  console.log(error.name)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message})
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+const port = process.env.PORT || 3989
+
+app.listen(process.env.PORT, () => {
   console.log(`listening on port ${port}`)
 })
 
